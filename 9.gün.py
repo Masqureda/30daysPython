9df5e2250dc57cccd7756199a7c097385121a750
Kadir Köroğlu <122994783+Masqureda@users.noreply.github.com>
2023-03-26 21:55:25 +0300

    Conditionals

---
+++ 9.gün.py
@@ -0,0 +1,44 @@
+sıcaklık = 25
+
+if sıcaklık > 30:
+    print("Hava çok sıcak!")
+elif sıcaklık > 20:
+    print("Hava sıcak.")
+else:
+    print("Hava soğuk.")
+
+   
+yaş = 18
+
+if yaş < 18:
+    print("Bu aktiviteye katılamazsın.")
+else:
+    print("Hoş geldin, aktiviteye katılabilirsin!")
+
+    
+    
+    sayı = 7
+
+if sayı > 0:
+    print("Sayı pozitiftir.")
+elif sayı == 0:
+    print("Sayı sıfırdır.")
+else:
+    print("Sayı negatiftir.")
+    
+    
+    kullanıcı_adı = "johndoe"
+
+if kullanıcı_adı == "johndoe":
+    print("Hoş geldin, John!")
+else:
+    print("Kim olduğunu tanıyamadım.")
+
+    
+    şifre = "abcd1234"
+
+if len(şifre) < 8:
+    print("Şifre çok kısa.")
+else:
+    print("Şifre kabul edildi.")
+
