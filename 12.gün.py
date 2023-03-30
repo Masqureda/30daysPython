d73129954d629d0ada8c4f79aaddf73d6d7e3d83
Kadir Köroğlu <122994783+Masqureda@users.noreply.github.com>
2023-03-30 22:53:08 +0300

    Functions

---
+++ 12.gün.py
@@ -0,0 +1,52 @@
+def selamla():
+    print("Merhaba!")
+    
+    
+    
+    
+def square(x):
+
+return x * x
+square(2)
+4
+a = square(2)
+a
+4
+
+
+
+
+def square(x):
+    
+    res = x * x
+    
+    return res
+    
+    print("Square of " + str(x) + ": " + str(res))
+    
+    
+    
+    
+x=int(input("f degeri gir :"))
+def f(x):
+    
+    res = x * x
+    
+    if x % 2 == 0:
+        
+        return res
+    else:
+        return res + 10
+    
+print("Square of " + str(x) + ": " + str(res))
+
+
+
+
+def square(x):
+    """
+    x'in karesini hesaplar
+    """
+    return x * x
+  
+  ?square ile çağrılabilir
