8a2eeadf958a92447d6091b3df2411ca9d56b27b
Kadir Köroğlu <122994783+Masqureda@users.noreply.github.com>
2023-04-02 01:28:13 +0300

    DailyPractices

---
+++ 13.gün..py
@@ -0,0 +1,11 @@
+x = int(input("1. sayiyi girin :"))
+y = int(input("2.sayiyi girin :"))
+sonuc = x+y
+print(sonuc, "toplamı")
+
+
+
+x = list(input("Metin gir :"))
+metin=x
+metin.reverse()
+print("Tersi :", metin)
