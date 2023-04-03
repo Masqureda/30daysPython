432bc90d5cccff59bb8a596b9a043d31b6e5866e
Kadir Köroğlu <122994783+Masqureda@users.noreply.github.com>
2023-04-03 23:32:05 +0300

    Function Samples

---
+++ 15.gün.py
@@ -0,0 +1,15 @@
+def topla(sayı1,sayı2):
+    return sayı1+sayı2
+topla = X 
+print(topla(3,5
+            
+            
+def min_max(sayilar):
+    en_kucuk = sayilar[0]
+    en_buyuk = sayilar[0]
+    for sayi in sayilar:
+        if sayi < en_kucuk:
+            en_kucuk = sayi
+        elif sayi > en_buyuk:
+            en_buyuk = sayi
+    return (en_kucuk, en_buyuk)
